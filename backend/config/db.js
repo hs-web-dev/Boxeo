15690376c475085cb53f69906eaa7edcaaa894cf
hs-web-dev <hadr.slno@gmail.com>
2026-03-29 14:43:41 +0200

    Add MongoDB connection function using mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connecté");
+    } catch (err) {
+        console.error("Erreur MongoDB :", err);
+        process.exit(1);
+    }
+}
 
