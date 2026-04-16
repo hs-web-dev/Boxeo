194ae9987e261153b356129640f665975a00ce0d
hs-web-dev <hadr.slno@gmail.com>
2026-04-16 21:54:46 +0200

    a

---
+++ frontend/garage.js
@@ -0,0 +1,25 @@
+const API_URL = "https://boxeo-p8t4.onrender.com/api";
+
+const params = new URLSearchParams(window.location.search);
+const garageId = params.get("id");
+
+async function loadGarage() {
+    const res = await fetch(`${API_URL}/garages/${garageId}`);
+    const g = await res.json();
+
+    document.getElementById("garageName").innerText = g.name;
+    document.getElementById("garageAddress").innerText = "Adresse : " + g.address;
+    document.getElementById("garagePlaces").innerText = "Places : " + g.places;
+    document.getElementById("garageType").innerText = "Type : " + g.type;
+
+    // Afficher la carte
+    const map = L.map("map").setView([g.lat, g.lng], 16);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+    L.marker([g.lat, g.lng]).addTo(map);
+}
+
+loadGarage();
+
+function goHome() {
+    window.location.href = "index.html";
+}
