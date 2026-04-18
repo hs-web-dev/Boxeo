986303638b4eb48cf3bf07f664c88544dcea5179
hs-web-dev <hadr.slno@gmail.com>
2026-04-18 13:41:30 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -28,9 +27,10 @@ app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// (optionnel) si tu gardes encore des fichiers locaux
+// === SERVE UPLOADS ===
 app.use("/uploads", express.static("uploads"));
 
+// === ROUTES API ===
 app.use("/api/auth", authRoutes);
 app.use("/api/garages", garageRoutes);
 app.use("/api/staff", staffRoutes);
